4acd5f1767303075e278c50aa1bea18fa9b80b3d
Lokender Chouhan <lokendrajdpr@gmail.com>
2025-04-20 02:28:37 +0530

    remove unnecessary build

--- vite.config.cjs
+++ vite.config.cjs
@@ -7,12 +7,12 @@ module.exports = defineConfig({
         main: './index.html',
         contact: './contact.html',
         // blog: './blog.html',
-        work: './work.html',
-        blog1: './blog1.html',
-        blog2: './blog2.html',
-        blog3: './blog3.html',
-        project1: './project1.html',
-        project2: './project2.html',
+        // work: './work.html',
+        // blog1: './blog1.html',
+        // blog2: './blog2.html',
+        // blog3: './blog3.html',
+        // project1: './project1.html',
+        // project2: './project2.html',
       }
     }
   }
